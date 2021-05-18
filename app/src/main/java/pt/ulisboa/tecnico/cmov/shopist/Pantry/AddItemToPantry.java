6e9333425f9e25762442f50a46ef657b1eb0d8a9
GenLike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2021-05-18 18:48:51 +0100

    fix namespace

--- app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/Pantry/AddItemToPantry.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/shopist/Pantry/AddItemToPantry.java
@@ -1,4 +1,4 @@
-package pt.ulisboa.tecnico.cmov.shopist;
+package pt.ulisboa.tecnico.cmov.shopist.Pantry;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
